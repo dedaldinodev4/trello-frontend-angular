7ad836b39ce8788cf1521d1080921b43ff44aa4a
Dedaldino Daniel <dedaldinodev4@gmail.com>
2022-09-01 14:29:12 +0100

    Done and update frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://trello-backend-rfto0aza3-dedaldinodev4.vercel.app/api',
+  sockectUrl: 'https://trello-backend-rfto0aza3-dedaldinodev4.vercel.app',
 };
